23bba8e2bff8b8cc4864eb4594ce77a539464726
Edu Caribé <28613091+caribeedu@users.noreply.github.com>
2023-12-17 12:16:16 -0300

    Add mAR and F1 evaluation

--- model/evaluate_model.py
+++ model/evaluate_model.py
@@ -1,51 +1,76 @@
-# import mask rcnn libraries
 from mrcnn.config import Config
-from mrcnn.utils import compute_ap
+from mrcnn.utils import compute_ap, compute_recall
 from mrcnn.model import load_image_gt
 from mrcnn.model import mold_image
+from sklearn import metrics
 
-# import numpy libraries
-from numpy import expand_dims
-from numpy import mean
+from numpy import expand_dims, mean, arange
 
-# import helpers
 from helpers import build_dataset, create_model
 
 class EvaluationConfig(Config):
     NAME = "evaluation"
     NUM_CLASSES = 1 + 51
-    DETECTION_MIN_CONFIDENCE = 0.85
+    DETECTION_MIN_CONFIDENCE = 0.8
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
 
-def compute_mean_avarage_precision(dataset, model, config):
-    APs = list()
-    for image_id in dataset.image_ids:
+def compute_ar(pred_boxes, gt_boxes, list_iou_thresholds):
+    AR = []
+    for iou_threshold in list_iou_thresholds:
 
-        image, _, gt_class_id, gt_bbox, gt_mask = load_image_gt(dataset, config, image_id)
+        try:
+            recall, _ = compute_recall(pred_boxes, gt_boxes, iou=iou_threshold)
 
-        scaled_image = mold_image(image, config)
+            AR.append(recall)
 
-        sample = expand_dims(scaled_image, 0)
+        except:
+          AR.append(0.0)
+          pass
 
-        r = model.detect(sample, verbose=1)[0]
+    AUC = 2 * (metrics.auc(list_iou_thresholds, AR))
+    return AUC
 
-        AP, _, _, _ = compute_ap(gt_bbox, gt_class_id, gt_mask, r["rois"], r["class_ids"], r["scores"], r['masks'])
+def evaluate_model(dataset, model, cfg, list_iou_thresholds=None):
 
-        APs.append(AP)
-    mAP = mean(APs)
-    return mAP
+  if list_iou_thresholds is None: list_iou_thresholds = arange(0.5, 1.01, 0.1)
+
+  APs = []
+  ARs = []
+  for image_id in dataset.image_ids:
+		
+    image, _, gt_class_id, gt_bbox, gt_mask = load_image_gt(dataset, cfg, image_id)
+		
+    scaled_image = mold_image(image, cfg)
+		
+    sample = expand_dims(scaled_image, 0)
+		
+    result = model.detect(sample, verbose=0)[0]
+		
+    AP, _, _, _ = compute_ap(gt_bbox, gt_class_id, gt_mask, result["rois"], result["class_ids"], result["scores"], result['masks'], iou_threshold=0.5)
+		
+    AR = compute_ar(result['rois'], gt_bbox, list_iou_thresholds)
+    ARs.append(AR)
+    APs.append(AP)
+
+  mAP = mean(APs)
+  mAR = mean(ARs)
+  f1_score = 2 * ((mAP * mAR) / (mAP + mAR))
+
+  return mAP, mAR, f1_score
 
 SETS_PATH = "/content/drive/MyDrive/TrabalhoA3"
 
 val_set = build_dataset("val", SETS_PATH)
 
-MODEL_WEIGHTS_PATH = SETS_PATH + '/signal_plates_mask_rcnn.h5'
+MODEL_WEIGHTS_PATH = SETS_PATH + '/signal_plates_mask_rcnn_.h5'
 
 config = EvaluationConfig()
 
 model = create_model('inference', config, MODEL_WEIGHTS_PATH)
 
-mAP = compute_mean_avarage_precision(val_set, model, config)
+mAP, mAR, f1 = evaluate_model(val_set, model, config)
 
 print("Mean Avarage Precision on validation dataset: %.3f" % mAP)
+print("Mean Avarage Recall on validation dataset: %.3f" % mAR)
+print("F1 Score on validation dataset: %.3f" % f1)
